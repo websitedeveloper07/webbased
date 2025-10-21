2de5c4e46e6648e8b33ae4db5ccf983d044e4b55
websitedeveloper07 <aadityamoga2@gmail.com>
2025-10-21 21:12:19 +0530

    Update adminaccess_panel.php

--- adminaccess_panel.php
+++ adminaccess_panel.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Admin access key (change this to your secure key)
-define('ADMIN_ACCESS_KEY', 'iloveyoupayalalways');
+define('ADMIN_ACCESS_KEY', 'YOUR_SECURE_ADMIN_KEY_123');
 
 // Maintenance flag file path
 define('MAINTENANCE_FLAG', 'maintenance.flag');
