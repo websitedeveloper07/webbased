0d1d3ca28b6e42d385df24ba16eb5c5649afbd1a
websitedeveloper07 <aadityamoga2@gmail.com>
2025-10-22 14:07:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -395,6 +395,9 @@ document.addEventListener('DOMContentLoaded', function() {
                 processData: false,
                 contentType: false,
                 timeout: 300000,
+                headers: {
+                    'a3lhIHJlIGxhd2RlIHlhaGkgb2thYXQgaGFpIGt5YSB0ZXJpIGtpIGR1c3JvIGthIGFwaSB1c2Uga3JuYSAxIGJhYXAga2EgaGFpIHRvIGtodWRrYSBibmEgaWRociBtdCB1c2Uga3Lwn5iC'
+                },               
                 xhr: function() {
                     const xhr = new window.XMLHttpRequest();
                     xhr.upload.addEventListener("progress", function(evt) {
