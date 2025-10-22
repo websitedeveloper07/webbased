7e674819840b1d50ab97f59ac7c628bf66f43f49
websitedeveloper07 <aadityamoga2@gmail.com>
2025-10-22 13:37:49 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,11 @@
+
 <?php
 // -------------------------------
-// SESSION MANAGER
+// SESSION & INITIAL CONFIG
 // -------------------------------
-require_once 'session_manager.php';
- $session = SessionManager::getInstance();
+ini_set('session.gc_maxlifetime', 3600);
+ini_set('session.cookie_lifetime', 3600);
+session_start();
 
 // -------------------------------
 // CONFIGURATION
