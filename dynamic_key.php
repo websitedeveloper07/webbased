6ff55a9ba1346137d4ddee3bf59ba93cc20cf8ea
websitedeveloper07 <aadityamoga2@gmail.com>
2025-10-23 19:40:49 +0530

    Create dynamic_key.php

---
+++ dynamic_key.php
@@ -0,0 +1,8 @@
+<?php
+header('Content-Type: application/javascript');
+
+// Put your new key here
+$apiKey = 'kyarelwdeaagyafirseidhrgaandmarwaneqV9nP3kR8xM1cF2jG6tY5uZ0oL4iW7A9fK3pQz7XvB1mNcD8rJ5wL2yT6sU0aEA4bH7qV9nP3kR8xM1cF2jG6tY5uZ0oL4iW7';
+
+// Output JS code with the key
+echo "const X_API_KEY = '{$apiKey}';";
