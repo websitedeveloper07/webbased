20aa40952e0b156ce652794d2151e6e423cbf766
websitedeveloper07 <aadityamoga2@gmail.com>
2025-10-24 11:27:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1609,16 +1609,6 @@ if (empty($userPhotoUrl)) {
                         </div>
                     </label>
                     <label class="gateway-option">
-                        <input type="radio" name="gateway" value="gate/stripegbp.php">
-                        <div class="gateway-option-content">
-                            <div class="gateway-option-name">
-                                <i class="fab fa-stripe"></i> Stripe 
-                                <span class="gateway-badge badge-charge">1£ Charge</span>
-                            </div>
-                            <div class="gateway-option-desc">Payment processing with £1 charge</div>
-                        </div>
-                    </label>
-                    <label class="gateway-option">
                         <input type="radio" name="gateway" value="gate/shopify1$.php">
                         <div class="gateway-option-content">
                             <div class="gateway-option-name">
