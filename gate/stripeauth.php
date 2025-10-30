a64bdf054932bb03d481c2942c2b45279a479175
websitedeveloper07 <aadityamoga2@gmail.com>
2025-10-31 02:16:14 +0530

    Update stripeauth.php

--- gate/stripeauth.php
+++ gate/stripeauth.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/globalstats.php';
 // Check if this is a GET request and show the HTML page immediately
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     header('Content-Type: text/html; charset=utf-8');
